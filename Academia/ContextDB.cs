5e5d17b23839e4201637bf72d4fa546db6a373ce
Valentino Nicola <110569291+Valenicola02@users.noreply.github.com>
2024-09-18 21:08:09 -0300

    ContextDB

---
+++ Academia/ContextDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Academia
+{
+    public class ContextDB
+    {
+        private string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=Academia;User ID=net;Password=net;";
+    }
+}
